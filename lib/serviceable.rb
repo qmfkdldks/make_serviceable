cab87df076194a5831e32cfb77918ca43e881e4b
Brian Kang <qmfkdldks@gmail.com>
2019-02-07 14:58:01 -0300

    Serviceable call and circuitbox added.

--- lib/serviceable.rb
+++ lib/serviceable.rb
@@ -2,5 +2,30 @@ require "serviceable/version"
 
 module Serviceable
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def call(*args)
+      circuit.run do
+        new(*args).call
+      end
+    end
+  end
+
+  # protected
+  def service_name
+    self.class.name
+  end
+
+  def circuit_options
+    {}
+  end
+
+  private
+  def circuit
+    Circuitbox.circuit(service_name, circuit_options)
+  end
 end
--- spec/serviceable_spec.rb
+++ spec/serviceable_spec.rb
@@ -1,9 +1,20 @@
+class TestService
+  include Serviceable
+end
+
 RSpec.describe Serviceable do
   it "has a version number" do
     expect(Serviceable::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:serivce_class) { TestService }
+
+  it "should add #call to shorten #new." do
+    expect(serivce_class).to respond_to(:call)
   end
+
+  context "instantiated" do
+
+  end
+
 end
