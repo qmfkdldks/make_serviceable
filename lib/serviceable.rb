4376871f187f21314bf2c2cbf1f9374aebfb7026
Brian Kang <qmfkdldks@gmail.com>
2019-02-07 12:44:28 -0300

    Initial commit.

---
+++ lib/serviceable.rb
@@ -0,0 +1,6 @@
+require "serviceable/version"
+
+module Serviceable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/serviceable/version.rb
@@ -0,0 +1,3 @@
+module Serviceable
+  VERSION = "0.1.0"
+end
